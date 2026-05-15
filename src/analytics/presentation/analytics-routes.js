1323c155dc92c7aed140ef3dcdca431216465e6b
LS <143048016+luccsss@users.noreply.github.com>
2026-05-15 06:35:38 -0500

    feat: add analytics routes for dashboard view

---
+++ src/analytics/presentation/analytics-routes.js
@@ -0,0 +1,7 @@
+const analyticsDashboard = () => import('./views/analytics-dashboard.vue');
+
+const analyticsRoutes = [
+    { path: 'dashboard', name: 'analytics-dashboard', component: analyticsDashboard, meta: { title: 'Analiticas', roles: ['rancher', 'veterinarian'] } }
+];
+
+export default analyticsRoutes;
