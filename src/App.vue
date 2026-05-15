bacc58bb41878771977fb347c08595560aebf23f
Jorge Ayala <jayala42000@gmail.com>
2026-05-15 04:29:37 -0500

    feat: updating template in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup></script>
+<script setup>
+import Layout from "./shared/presentation/components/layout.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <layout/>
 </template>
-
-<style scoped></style>
