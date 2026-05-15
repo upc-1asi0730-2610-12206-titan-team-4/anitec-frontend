47e6e8bda2153e6819d5bda2e38cb376d6b5f45c
LS <143048016+luccsss@users.noreply.github.com>
2026-05-15 06:31:24 -0500

    feat: create analytics.store.js for metrics management
    Implement analytics store to manage metrics and errors.

---
+++ src/analytics/application/analytics.store.js
@@ -0,0 +1,33 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+import {AnalyticsApi} from "../infrastructure/analytics-api.js";
+import {AnalyticsMetricAssembler} from "../infrastructure/analytics-metric.assembler.js";
+
+const api = new AnalyticsApi();
+
+/**
+ * Store that manages the metrics used in analytics.
+ */
+const useAnalyticsStore = defineStore('analytics', () => {
+
+    const metrics = ref([]);
+
+    const errors = ref([]);
+
+    const loaded = ref(false);
+
+    /**
+     * Loads metrics from the API.
+     * @returns {Promise}
+     */
+    function fetchMetrics() {
+        return api.getMetrics().then(response => {
+            metrics.value = AnalyticsMetricAssembler.toEntitiesFromResponse(response);
+            loaded.value = true;
+        }).catch(error => errors.value.push(error));
+    }
+
+    return { metrics, errors, loaded, fetchMetrics };
+});
+
+export default useAnalyticsStore;
