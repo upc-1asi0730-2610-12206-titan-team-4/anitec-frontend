14d3871b84181516f76e142a10e9514940e6205e
Jorge Ayala <jayala42000@gmail.com>
2026-05-15 04:44:15 -0500

    feat: refactor formatting in vue.app

--- src/main.js
+++ src/main.js
@@ -1,20 +1,38 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './app.vue'
-import PrimeVue from 'primevue/config';
-import Material from '@primeuix/themes/material';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./app.vue";
+import PrimeVue from "primevue/config";
+import Material from "@primeuix/themes/material";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
 import {
     Button,
     Card,
     Checkbox,
     Column,
     ConfirmationService,
-    ConfirmDialog, DataTable, Dialog,
-    DialogService, Drawer, FileUpload, FloatLabel, IconField, InputIcon, InputNumber, InputText, Menu,
-    Rating, Row, Select, SelectButton, Tag, Textarea, Toast,
-    ToastService, Toolbar, Tooltip
+    ConfirmDialog,
+    DataTable,
+    Dialog,
+    DialogService,
+    Drawer,
+    FileUpload,
+    FloatLabel,
+    IconField,
+    InputIcon,
+    InputNumber,
+    InputText,
+    Menu,
+    Rating,
+    Row,
+    Select,
+    SelectButton,
+    Tag,
+    Textarea,
+    Toast,
+    ToastService,
+    Toolbar,
+    Tooltip,
 } from "primevue";
 import Chart from "primevue/chart";
 import i18n from "./i18n.js";
@@ -25,33 +43,33 @@ createApp(App)
     .use(pinia)
     .use(router)
     .use(i18n)
-    .use(PrimeVue, { theme: { preset: Material}, ripple: true})
+    .use(PrimeVue, { theme: { preset: Material }, ripple: true })
     .use(ConfirmationService)
     .use(DialogService)
     .use(ToastService)
-    .component('pv-button', Button)
-    .component('pv-card', Card)
-    .component('pv-chart', Chart)
-    .component('pv-column', Column)
-    .component('pv-confirm-dialog', ConfirmDialog)
-    .component('pv-checkbox', Checkbox)
-    .component('pv-data-table', DataTable)
-    .component('pv-dialog', Dialog)
-    .component('pv-drawer', Drawer)
-    .component('pv-file-upload', FileUpload)
-    .component('pv-float-label', FloatLabel)
-    .component('pv-icon-field', IconField)
-    .component('pv-input-icon', InputIcon)
-    .component('pv-input-number', InputNumber)
-    .component('pv-input-text', InputText)
-    .component('pv-menu', Menu)
-    .component('pv-rating', Rating)
-    .component('pv-row', Row)
-    .component('pv-select', Select)
-    .component('pv-select-button', SelectButton)
-    .component('pv-tag', Tag)
-    .component('pv-textarea', Textarea)
-    .component('pv-toast', Toast)
-    .component('pv-toolbar', Toolbar)
-    .directive('tooltip', Tooltip)
-    .mount('#app')
+    .component("pv-button", Button)
+    .component("pv-card", Card)
+    .component("pv-chart", Chart)
+    .component("pv-column", Column)
+    .component("pv-confirm-dialog", ConfirmDialog)
+    .component("pv-checkbox", Checkbox)
+    .component("pv-data-table", DataTable)
+    .component("pv-dialog", Dialog)
+    .component("pv-drawer", Drawer)
+    .component("pv-file-upload", FileUpload)
+    .component("pv-float-label", FloatLabel)
+    .component("pv-icon-field", IconField)
+    .component("pv-input-icon", InputIcon)
+    .component("pv-input-number", InputNumber)
+    .component("pv-input-text", InputText)
+    .component("pv-menu", Menu)
+    .component("pv-rating", Rating)
+    .component("pv-row", Row)
+    .component("pv-select", Select)
+    .component("pv-select-button", SelectButton)
+    .component("pv-tag", Tag)
+    .component("pv-textarea", Textarea)
+    .component("pv-toast", Toast)
+    .component("pv-toolbar", Toolbar)
+    .directive("tooltip", Tooltip)
+    .mount("#app");
