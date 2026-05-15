f4edb288b2182c9820d5b1436c332d8460b6af7a
Melga1502 <melgarejojosep@gmail.com>
2026-05-15 06:23:29 -0500

    feat: add Animal class to represent farm animals
    This class represents an animal in a farm or herd with properties such as id, tag, name, species, breed, gender, birth date, weight, status, and herd ID.

---
+++ src/livestock/domain/model/herd.entity.js
@@ -0,0 +1,27 @@
+/**
+ * Represents an animal registered inside a farm or herd.
+ */
+export class Animal {
+
+    /**
+     * Creates an animal with its main data and converts numeric values when needed.
+     * @param {Object} animal Animal data.
+     */
+    constructor({ id = null, tag = '', name = '', species = 'Bovino', breed = '', gender = '', birthDate = '', weight = 0, status = 'Saludable', herdId = null }) {
+        this.id = id;
+        this.tag = tag;
+        this.name = name;
+        this.species = species;
+        this.breed = breed;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.weight = Number(weight);
+        this.status = status;
+
+        if (herdId) {
+            this.herdId = Number(herdId);
+        } else {
+            this.herdId = null;
+        }
+    }
+}
